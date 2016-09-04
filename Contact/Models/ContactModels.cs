f393280a8e890e93abc15d8acaf1f38e7e3810f0
daniel <carterdanielr@gmail.com>
2016-09-03 23:11:21 -0400

    client side form validation complete

---
+++ Contact/Models/ContactModels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contact.Models
+{
+    public class ContactModels
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+    }
+}
